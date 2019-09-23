d9b7888aa32e6772e1c5420f9e19fefb716d6958
Tiagospem <tiagospem@gmail.com>
2019-09-23 15:34:49 -0300

    add mailtrap and forgot password

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,7 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+Route.post('passwords', 'ForgotPasswordController.store')
